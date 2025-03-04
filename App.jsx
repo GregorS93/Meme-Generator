2baf37d34648bcffc247ecb24ca48f61905c2e12
GregorS93 <146366523+GregorS93@users.noreply.github.com>
2025-03-04 14:13:15 +0100

    Initial commit

---
+++ App.jsx
@@ -0,0 +1,11 @@
+import Header from "./Header.Component/Header";
+import Main from "./Main.Component/Main";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
+}
