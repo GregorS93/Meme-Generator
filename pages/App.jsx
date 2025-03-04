2af54a9001080309d55cb7d7a41731acb0f3acb6
GregorS93 <146366523+GregorS93@users.noreply.github.com>
2025-03-04 14:41:01 +0100

    Folder structure organizing

---
+++ pages/App.jsx
@@ -0,0 +1,11 @@
+import Header from "../components/Header/Header-component";
+import Main from "../components/Main/Main-component";
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+  );
+}
